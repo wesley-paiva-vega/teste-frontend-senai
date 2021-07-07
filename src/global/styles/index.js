16b1a8fc0a7cbc685e32d139be3b15f46ee61559
wesley-snybool-dev <wesley.snybool.dev@gmail.com>
2021-07-07 11:08:30 -0300

    My two commit

---
+++ src/global/styles/index.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+ const GlobalStyle = createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        html {
+            font-size: 62.6%;
+        }
+
+        body {
+            background-color: lightgrey;
+        }
+    }
+`
+
+export default GlobalStyle;
